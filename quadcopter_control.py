338e3420430a0b8a55248cada450be0db6228c20
Appolo-11 <36989596+Appolo-11@users.noreply.github.com>
2020-01-26 17:41:50 +0300

    simple version with no extra comment data
    it's simple algorithm with no bags' ampunt checking.

--- quadcopter_control.py
+++ quadcopter_control.py
@@ -1,4 +1,6 @@
-# main script
+# In this code it's tried to go along defined trajectories and detect bags and their order. 
+# Bags detection - is detection of circles from camera image and their colour definition.
+#  
 
 import sim as vrep
 import time
@@ -23,6 +25,7 @@ amount_of_bags = 0
 path = '/home/evgesha/nti/copter_proj/imgs'
 result = []
 
+#thresholds for three different colours
 low_green = (76, 152, 0)
 high_green = (145, 255, 24)
 
@@ -32,11 +35,14 @@ high_yellow = (116, 255, 255)
 low_purple = (150, 70, 150)
 high_purple = (255, 145, 255) 
 
-colour_defined = False
+#possible colours
 colours = ['green', 'yellow', 'purple']
 current_colour = None
 
+# colour_rad[colour] = list of finded circles' radiuses for this colour
 colour_rad = {'green': [], 'yellow': [], 'purple': [] }
+# colour_count[color] = absolute number of last detected circle in this colour.
+# by that we anderstand the order : color with max value - last bags, min value - first
 colour_count = {'green': 0, 'yellow': 0, 'purple': 0 }
 
 check = {   'green': lambda x: is_green(x), 
@@ -47,7 +53,7 @@ counter = 0
 
 
 
-
+#go to x1 position along x-axis
 def go_to_x(x1, action = None):
     global x, y, z, start_stream
 
@@ -92,7 +98,7 @@ def go_to_x(x1, action = None):
     x = x1
     start_stream = False
 
-
+#go to y1 position along y-axis
 def go_to_y(y1):
     global x, y, z
 
@@ -128,6 +134,7 @@ def go_to_y(y1):
 
     y = y1
 
+#go to z1 position along z-axis
 def go_to_z(z1):
     global x, y, z
 
@@ -152,9 +159,7 @@ def go_to_z(z1):
         z -= sign*0.01
         print(z)
         print('\n')
-        # returnCode, linearVelocity, angularVelocity=vrep.simxGetObjectVelocity(clientID, QuadricopterT, vrep.simx_opmode_streaming)
-        # returnCode, linearVelocity, angularVelocity=vrep.simxGetObjectVelocity(clientID, QuadricopterT, vrep.simx_opmode_buffer)
-
+       
     err = vrep.simxSetObjectPosition(
             clientID, QuadricopterT, -1, (x, y, z1), vrep.simx_opmode_oneshot_wait)
 
@@ -164,7 +169,7 @@ def go_to_z(z1):
     z = z1
 
 
-
+#circle motion with defined height and radius
 def circle_move(radius, height):
 
     go_to_z(height)
@@ -186,7 +191,7 @@ def circle_move(radius, height):
         err, Qpos = vrep.simxGetObjectPosition(clientID, QuadricopterT, -1, vrep.simx_opmode_oneshot_wait)
         print(Qpos[0]**2 + Qpos[1]**2)
 
-
+#square motion with defined height and square
 def square_move(square, height):
 
     err, Qpos = vrep.simxGetObjectPosition(clientID, QuadricopterT, -1, vrep.simx_opmode_oneshot_wait)
@@ -220,7 +225,7 @@ def square_move(square, height):
         go_to_x(x)
         
 
-
+#spiral motion with defined height and radius (radius decreased as r/1.001)
 def spiral_move(radius, height):
     
     err, Qpos = vrep.simxGetObjectPosition(clientID, QuadricopterT, -1, vrep.simx_opmode_oneshot_wait)
@@ -248,6 +253,7 @@ def spiral_move(radius, height):
         print(Qpos[0]**2 + Qpos[1]**2)
 
 
+# gain camera image, detect circle with max radius, define its colour and add info to colour_rad and colour_count
 def detect_bag(action = None):
 
     global start_stream, amount_of_bags, bags, last_r, result
@@ -275,23 +281,21 @@ def detect_bag(action = None):
 
         newImg = np.array(image, dtype=np.uint8)
         newImg.resize([resolution[0], resolution[1], 3])
-        # print('avg_color = ', avg_color)
             
 
         gray = cv2.cvtColor(newImg, cv2.COLOR_BGR2GRAY)
         gray = cv2.medianBlur(gray, 3)
-        # image = newImg
+
         output = newImg
-        # print(output.shape)
+
 
         # detect circles in the image
         circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 2.1, 25)
+
         # ensure at least some circles were found
         if circles is not None:
             print('hey')
 
-            # convert the (x, y) coordinates and radius of the circles to integers
-            # circles = np.round(circles[0, :]).astype("int")
             circles = circles[0, :]
             r_max = 0
             # loop over the (x, y) coordinates and radius of the circles
@@ -302,8 +306,7 @@ def detect_bag(action = None):
                     r_max = r
                     x_max = x
                     y_max = y
-                # draw the circle in the output image, then draw a rectangle
-                # corresponding to the center of the circle
+               
             
             r_max = int(r_max)
             x_max = int(x_max)
@@ -311,9 +314,7 @@ def detect_bag(action = None):
 
             # cv2.circle(output, (x_max, y_max), r_max, (0, 255, 0), 4)
             # cv2.rectangle(output, (x_max - 5, y_max - 5), (x_max + 5, y_max + 5), (0, 128, 255), -1)  
-            sector = output[y_max-5:y_max+5, x_max-5:x_max+5]    
-            avg_color_per_row = np.average(sector, axis=0)
-            avg_color = np.average(avg_color_per_row, axis=0)
+            sector = output[y_max-5:y_max+5, x_max-5:x_max+5] 
 
             detect_colour(sector)
 
@@ -322,34 +323,15 @@ def detect_bag(action = None):
                 colour_count[current_colour] = counter
                 counter += 1
 
-            # print(avg_color, type(avg_color))
+#             avg_color_per_row = np.average(sector, axis=0)
+#             avg_color = np.average(avg_color_per_row, axis=0)
+
+#             name = str(amount_of_bags) + '____' + str(avg_color) + '.png'
+#             # show the output image
+#             cv2.imwrite(os.path.join(path , name), sector)
 
 
-
-            # if colour_defined and is_colour_changed(sector):
-            #     result.append(max(bags[:-1]))
-            #     bags = [bags[-1]]
-            #     print(result)
-            #     detect_colour(sector)
-            #     print('colour changed!', current_colour)
-            # else:
-            #     if len(result) == 2 and counter > 4:
-            #         result.append(max(bags))
-            #     detect_colour(sector)
-                
-        
-
-            avg_color_per_row = np.average(sector, axis=0)
-            avg_color = np.average(avg_color_per_row, axis=0)
-
-            name = str(amount_of_bags) + '____' + str(avg_color) + '.png'
-            # show the output image
-            cv2.imwrite(os.path.join(path , name), sector)
-
-
-
-
-
+#check changed colour of circles or not
 def is_colour_changed(sector):
 
     global current_colour
@@ -364,7 +346,7 @@ def is_colour_changed(sector):
         print('colour is changed')
         return True
 
-
+#detect colour of finded circles
 def detect_colour(sector):
 
     global current_colour, colour_defined, colours
@@ -387,7 +369,7 @@ def detect_colour(sector):
     colour_defined = False
     print('no colour')
 
-
+#check is sector green or not
 def is_green(sector):
     green_mask = cv2.inRange(sector, low_green, high_green)
     if abs(get_avrg_colour(green_mask) - 255) <= 10:
@@ -412,7 +394,7 @@ def is_purple(sector):
     else:
         return False
 
-
+#count an average color of the sector
 def get_avrg_colour(sector):
     avg_color_per_row = np.average(sector, axis=0)
     avg_color = np.average(avg_color_per_row, axis=0)
@@ -494,13 +476,6 @@ print('colour_count = ', colour_count)
 # time.sleep(4)
 
 
-err, Qpos = vrep.simxGetObjectPosition(
-    clientID, QuadricopterT, -1, vrep.simx_opmode_oneshot_wait)
-
-print('Qpos =' , Qpos)
-
-
-
 # Throw ball
 # emptyBuff = bytearray()
 # res, retInts, retFloats, retStrings, retBuffer = vrep.simxCallScriptFunction(clientID, 'Quadricopter', vrep.sim_scripttype_childscript,
@@ -519,34 +494,6 @@ print('Qpos =' , Qpos)
 # # convert byte array to image
 # newImg = np.array(image, dtype=np.uint8)
 # newImg.resize([resolution[0], resolution[1], 3])
-
-# gray = cv2.cvtColor(newImg, cv2.COLOR_BGR2GRAY)
-# image = newImg
-# output = image.copy()
-
-# # detect circles in the image
-# circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 2, 20)
-# print(circles)
- 
-# # ensure at least some circles were found
-# if circles is not None:
-#     print('hey')
-#     # convert the (x, y) coordinates and radius of the circles to integers
-#     circles = np.round(circles[0, :]).astype("int")
- 
-#     # loop over the (x, y) coordinates and radius of the circles
-#     for (x, y, r) in circles:
-#         # draw the circle in the output image, then draw a rectangle
-#         # corresponding to the center of the circle
-#         cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-#         cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
- 
-#     # show the output image
-#     cv2.imwrite("output1.png", np.hstack([image, output]))
-
-
-
-
 # # save
 # cv2.imwrite('image.png', image)
 # print('Image is saved')
